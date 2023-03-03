6e51c7f068bf7d550105c4ec21dd02724d33baea
Jocheu <88211597+Jocheu@users.noreply.github.com>
2023-03-03 14:53:09 +0100

    Sam nie wiem

--- app/src/main/java/com/example/toolbars/MainActivity.kt
+++ app/src/main/java/com/example/toolbars/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.toolbars
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.title = "Olivier van Overveld"
+        setSupportActionBar(toolbar)
+
+    }
+}
+
+class Drugi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
+
+
+class Trzeci : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
     }
 }
